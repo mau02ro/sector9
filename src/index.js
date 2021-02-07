50b7b35419f2ddaf15bf2862288e31afdcaf601e
Mauro Hernandez <mauroDeveloper@hotmail.com>
2021-02-06 20:34:27 -0400

    Configuración basica de redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk'
 
+import reducers from './reducers'
 import App from './App'
-// TODO: Config redux
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
