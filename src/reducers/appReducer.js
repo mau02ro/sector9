e162e1eab1317df23b39c2fa632a32217fd98fe6
Mauro Hernandez <mauroDeveloper@hotmail.com>
2021-04-20 15:44:22 -0400

    funcionalidad de shoppingCart en el reducer

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,11 +1,20 @@
+import { SHOPPING_CART_TYPE } from "../types/appReducer";
+
 const INITIAL_STATE = {
+  shoppingCart: [],
   loading: false,
   error: null,
-}
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case SHOPPING_CART_TYPE:
+      return {
+        ...state,
+        shoppingCart: [...state.shoppingCart, action.payload],
+      };
+
     default:
-      return state
+      return state;
   }
-}
+};
