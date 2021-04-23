91a4f2288983f831f8e8c0c5586af89b5c9508b5
Mauro Hernandez <mauroDeveloper@hotmail.com>
2021-04-23 19:31:13 -0400

    Bug fixed

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   mode: "production",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     publicPath: "/",
   },
