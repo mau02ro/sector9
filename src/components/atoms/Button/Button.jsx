8c591eebdc75555f33675c5b6d1e3ace0cce841f
Mauro Hernandez <mauroDeveloper@hotmail.com>
2021-04-24 00:58:59 -0400

    Refactorización de Button

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import "./button.css";
 
-const Button = ({ children, action, type = "primary" }) => {
+const Button = ({ children, action, type }) => {
   return (
     <button type="button" className={`button button-${type}`} onClick={action}>
       {children}
@@ -11,12 +12,32 @@ const Button = ({ children, action, type = "primary" }) => {
   );
 };
 
-const Button_Link = ({ href = "#", children, type = "primary" }) => {
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
+  type: PropTypes.string,
+};
+
+Button.defaultProps = {
+  type: "primary",
+};
+
+const Button_Link = ({ href, children, type = "primary" }) => {
   return (
     <Link to={href} className={`button button-${type}`}>
       {children}
     </Link>
   );
+};
+
+Button_Link.propTypes = {
+  children: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
+
+Button_Link.defaultProps = {
+  type: "primary",
 };
 
 Button.Link = Button_Link;
