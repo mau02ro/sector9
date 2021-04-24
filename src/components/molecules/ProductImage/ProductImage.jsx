5af9a113e9feca63d34d44384bb3a262f4a84d67
Mauro Hernandez <mauroDeveloper@hotmail.com>
2021-04-24 02:57:11 -0400

    Refactorización de ProductImage

--- src/components/molecules/ProductImage/ProductImage.jsx
+++ src/components/molecules/ProductImage/ProductImage.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import classNames from "classnames";
+import PropTypes from "prop-types";
 
 import Image from "../../atoms/Image";
 
 import "./productImage.css";
 
-const ProductImage = ({ images }) => {
+const ProductImage = ({ images, name }) => {
   const [index, setIndex] = useState(0);
 
   return (
     <div className="productImage">
-      <Image src={images[index]} />
+      <Image src={images[index]} alt={`Product - ${name}`} />
       <div className="productImage_options">
         {images.length > 1 &&
           images.map((image, key) => (
@@ -21,12 +22,17 @@ const ProductImage = ({ images }) => {
               key={`productImage_options-${key}`}
               onClick={() => setIndex(key)}
             >
-              <Image src={image} />
+              <Image src={image} alt={`Product - ${name}`} />
             </div>
           ))}
       </div>
     </div>
   );
+};
+
+ProductImage.propTypes = {
+  images: PropTypes.array.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default ProductImage;
