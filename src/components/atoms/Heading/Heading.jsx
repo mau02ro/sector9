4f4262d4d06810474427197c77ebce447c906c08
Mauro Hernandez <mauroDeveloper@hotmail.com>
2021-04-24 01:10:20 -0400

    Refactorización de Heading

--- src/components/atoms/Heading/Heading.jsx
+++ src/components/atoms/Heading/Heading.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-import "./heading.css";
+import "./Heading.css";
 
-const Heading = ({ children, type = "primary", className = "" }) => {
+const Heading = ({ children, type, className }) => {
   return <h2 className={`heading heading-${type} ${className}`}>{children}</h2>;
+};
+
+Heading.propTypes = {
+  children: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Greeting.defaultProps = {
+  type: "primary",
+  className: "",
 };
 
 export default Heading;
